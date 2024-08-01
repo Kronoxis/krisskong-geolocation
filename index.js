6278f0eb491d66e3a7d54d52a8fc654925bfa4ca
AER-Quinten <q.wuyts@aeroplane.ae>
2024-08-02 01:19:30 +0200

    Add Speedometer application

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ require("./server.js");
 
 // Geolocation Transmitter
 require("./src/transmitter/transmitter.js");
+
+// Speedometer Application
+require("./src/speedometer/speedometer.js");
---
+++ src/speedometer/speedometer.js
@@ -0,0 +1,93 @@
+const server = require("../../server.js");
+
+// Overlay
+server.route(function (fastify) {
+    fastify.get("/speedometer", function (request, reply) {
+        return reply.view("/src/speedometer/speedometer.hbs", { SOCKET: process.env.SOCKET });
+    })
+});
+
+// API
+server.route(function (fastify) {
+    fastify.get("/speed", function (request, reply) {
+        return reply.code(200).send(speed);
+    });
+});
+
+// Calculate Speed
+let speed = 0;
+server.onLocationChanged(function (server, client, data) {
+    const distance = haversine(data.latitude, data.longitude);
+    const delta = deltaTime(data.time);
+    if (delta > toHours(10)) {
+        speed = distance / delta;
+    }
+
+    console.log(`${new Date().toLocaleTimeString()} ${speed.toFixed(2)}km/h`);
+
+    server.clients.forEach(target => {
+        if (target === client) return;
+        target.send(JSON.stringify({ type: "speed", speed }));
+    })
+});
+
+const last = {
+    valid: false,
+    latitude: 0,
+    longitude: 0,
+    time: -1
+};
+
+function toRadians(degrees) {
+    return degrees / 180 * Math.PI;
+}
+
+function toHours(milliseconds) {
+    return milliseconds / 3600000;
+}
+
+/**
+ * Haversine Distance Formula between last and current position
+ * @param {number} latitude Latitude in degrees
+ * @param {number} longitude Longitude in degrees
+ * @returns Haversine distance in kilometers
+ */
+function haversine(latitude, longitude) {
+    let distance = 0;
+    if (last.valid) {
+        const R = 6371;
+        const lat1 = toRadians(last.latitude);
+        const lat2 = toRadians(latitude);
+        const lon1 = toRadians(last.longitude);
+        const lon2 = toRadians(longitude);
+        const sdLat = Math.sin((lat2 - lat1) / 2);
+        const sdLon = Math.sin((lon2 - lon1) / 2);
+        const cLat1 = Math.cos(lat1);
+        const cLat2 = Math.cos(lat2);
+        const a = sdLat * sdLat + cLat1 * cLat2 * sdLon * sdLon;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        distance = R * c;
+    }
+
+    last.latitude = latitude;
+    last.longitude = longitude;
+    last.valid = true;
+
+    return distance;
+}
+
+/**
+ * Time between last and current update
+ * @param {number} time Current time
+ * @returns Delta Time in hours
+ */
+function deltaTime(time) {
+    let delta = 0;
+    if (time > 0) {
+        delta = toHours(time - last.time);
+    }
+
+    last.time = time;
+
+    return delta;
+}
