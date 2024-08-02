0bf2105090d05077444b1b18b30841b93d9f91d9
AER-Quinten <q.wuyts@aeroplane.ae>
2024-08-02 11:27:20 +0200

    Add preview

--- src/minimap/minimap.js
+++ src/minimap/minimap.js
@@ -3,6 +3,9 @@ const server = require("../../server.js");
 // Overlay
 server.route(function (fastify) {
     fastify.get("/minimap", function (request, reply) {
-        return reply.view("/src/minimap/minimap.hbs", { SOCKET: process.env.SOCKET });
+        return reply.view("/src/minimap/minimap.hbs", { 
+            URL: env.DEBUG ? "http://localhost:3000" : `https://${env.PROJECT}.glitch.me`,
+            SOCKET: env.DEBUG ? "ws://localhost:3000" : `wss://${env.PROJECT}.glitch.me`
+        });
     })
 });
